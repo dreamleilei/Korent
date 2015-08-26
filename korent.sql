0d46b0d67070942d0fc01c0dabb2e8a913541f3d
lei <1679211339@qq.com>
2015-08-26 16:34:09 +0800

    new sql

--- korent.sql
+++ korent.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.26, for Win64 (x86_64)
+-- MySQL dump 10.14  Distrib 5.5.41-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: korent
 -- ------------------------------------------------------
--- Server version	5.6.26
+-- Server version	5.5.41-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -24,8 +24,8 @@ DROP TABLE IF EXISTS `admin`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `admin` (
   `aid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(40) NOT NULL,
   PRIMARY KEY (`aid`),
   UNIQUE KEY `UK_5cca88c6i17ttuegcvdkgehah` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -48,12 +48,12 @@ DROP TABLE IF EXISTS `follow`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `follow` (
-  `rid` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
-  KEY `FK_r3awpfjt8kgdw4hrokne1lrsu` (`uid`),
+  `rid` int(11) NOT NULL,
   KEY `FK_m3snfbnyeimqngtt5ie8wamt` (`rid`),
-  CONSTRAINT `FK_m3snfbnyeimqngtt5ie8wamt` FOREIGN KEY (`rid`) REFERENCES `recent` (`rid`),
-  CONSTRAINT `FK_r3awpfjt8kgdw4hrokne1lrsu` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+  KEY `FK_r3awpfjt8kgdw4hrokne1lrsu` (`uid`),
+  CONSTRAINT `FK_r3awpfjt8kgdw4hrokne1lrsu` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`),
+  CONSTRAINT `FK_m3snfbnyeimqngtt5ie8wamt` FOREIGN KEY (`rid`) REFERENCES `recent` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -75,9 +75,9 @@ DROP TABLE IF EXISTS `notify`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `notify` (
   `nid` int(11) NOT NULL AUTO_INCREMENT,
-  `ToId` int(11) DEFAULT NULL,
-  `content` varchar(255) DEFAULT NULL,
-  `status` int(11) DEFAULT NULL,
+  `ToId` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
   PRIMARY KEY (`nid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -92,13 +92,13 @@ LOCK TABLES `notify` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `picturepath`
+-- Table structure for table `picturePath`
 --
 
-DROP TABLE IF EXISTS `picturepath`;
+DROP TABLE IF EXISTS `picturePath`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `picturepath` (
+CREATE TABLE `picturePath` (
   `rid` int(11) NOT NULL,
   `path` varchar(255) DEFAULT NULL,
   `id` int(11) NOT NULL,
@@ -108,12 +108,12 @@ CREATE TABLE `picturepath` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `picturepath`
+-- Dumping data for table `picturePath`
 --
 
-LOCK TABLES `picturepath` WRITE;
-/*!40000 ALTER TABLE `picturepath` DISABLE KEYS */;
-/*!40000 ALTER TABLE `picturepath` ENABLE KEYS */;
+LOCK TABLES `picturePath` WRITE;
+/*!40000 ALTER TABLE `picturePath` DISABLE KEYS */;
+/*!40000 ALTER TABLE `picturePath` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -125,12 +125,12 @@ DROP TABLE IF EXISTS `recent`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `recent` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `city` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
-  `district` varchar(255) DEFAULT NULL,
-  `province` varchar(255) DEFAULT NULL,
-  `classify` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `city` varchar(20) NOT NULL,
+  `country` varchar(20) NOT NULL,
+  `district` varchar(20) NOT NULL,
+  `province` varchar(20) NOT NULL,
+  `classify` varchar(255) NOT NULL,
+  `description` varchar(3000) NOT NULL,
   `status` int(11) DEFAULT NULL,
   `updateDate` date DEFAULT NULL,
   `uid` int(11) DEFAULT NULL,
@@ -158,11 +158,11 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `phone` varchar(255) DEFAULT NULL,
-  `qq` varchar(255) DEFAULT NULL,
+  `email` varchar(30) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `phone` varchar(16) DEFAULT NULL,
+  `qq` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `UK_gj2fy3dcix7ph7k8684gka40c` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -186,4 +186,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-25 21:18:31
+-- Dump completed on 2015-08-26 16:33:48
