f1a9ad9601a9822b74042a72173b13680a277fde
lei <1679211339@qq.com>
2015-08-29 14:08:02 +0800

    korent.sql

--- korent.sql
+++ korent.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.14  Distrib 5.5.41-MariaDB, for Linux (x86_64)
+-- MySQL dump 10.14  Distrib 5.5.44-MariaDB, for Linux (x86_64)
 --
 -- Host: localhost    Database: korent
 -- ------------------------------------------------------
--- Server version	5.5.41-MariaDB-log
+-- Server version	5.5.44-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -52,8 +52,8 @@ CREATE TABLE `follow` (
   `rid` int(11) NOT NULL,
   KEY `FK_m3snfbnyeimqngtt5ie8wamt` (`rid`),
   KEY `FK_r3awpfjt8kgdw4hrokne1lrsu` (`uid`),
-  CONSTRAINT `FK_r3awpfjt8kgdw4hrokne1lrsu` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`),
-  CONSTRAINT `FK_m3snfbnyeimqngtt5ie8wamt` FOREIGN KEY (`rid`) REFERENCES `recent` (`rid`)
+  CONSTRAINT `FK_m3snfbnyeimqngtt5ie8wamt` FOREIGN KEY (`rid`) REFERENCES `rent` (`rid`),
+  CONSTRAINT `FK_r3awpfjt8kgdw4hrokne1lrsu` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -103,7 +103,7 @@ CREATE TABLE `picturePath` (
   `path` varchar(255) DEFAULT NULL,
   `id` int(11) NOT NULL,
   PRIMARY KEY (`rid`,`id`),
-  CONSTRAINT `FK_bnlno6jrtky9i6lf56c7t76xx` FOREIGN KEY (`rid`) REFERENCES `recent` (`rid`)
+  CONSTRAINT `FK_bnlno6jrtky9i6lf56c7t76xx` FOREIGN KEY (`rid`) REFERENCES `rent` (`rid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -117,13 +117,13 @@ LOCK TABLES `picturePath` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `recent`
+-- Table structure for table `rent`
 --
 
-DROP TABLE IF EXISTS `recent`;
+DROP TABLE IF EXISTS `rent`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `recent` (
+CREATE TABLE `rent` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
   `city` varchar(20) NOT NULL,
   `country` varchar(20) NOT NULL,
@@ -131,22 +131,27 @@ CREATE TABLE `recent` (
   `province` varchar(20) NOT NULL,
   `classify` varchar(255) NOT NULL,
   `description` varchar(3000) NOT NULL,
+  `priceDescription` varchar(30) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `updateDate` date DEFAULT NULL,
-  `uid` int(11) DEFAULT NULL,
+  `ownerId` int(11) DEFAULT NULL,
+  `orderId` int(11) DEFAULT NULL,
   PRIMARY KEY (`rid`),
-  KEY `FK_8k8anpo0o578lo0yk676btdp0` (`uid`),
-  CONSTRAINT `FK_8k8anpo0o578lo0yk676btdp0` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `FK_1ogcvngy59ny1gbk24tskpsmr` (`ownerId`),
+  KEY `FK_4uxmjp03a1lf0plfj0b2ji8nv` (`orderId`),
+  CONSTRAINT `FK_1ogcvngy59ny1gbk24tskpsmr` FOREIGN KEY (`ownerId`) REFERENCES `user` (`uid`),
+  CONSTRAINT `FK_4uxmjp03a1lf0plfj0b2ji8nv` FOREIGN KEY (`orderId`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `recent`
+-- Dumping data for table `rent`
 --
 
-LOCK TABLES `recent` WRITE;
-/*!40000 ALTER TABLE `recent` DISABLE KEYS */;
-/*!40000 ALTER TABLE `recent` ENABLE KEYS */;
+LOCK TABLES `rent` WRITE;
+/*!40000 ALTER TABLE `rent` DISABLE KEYS */;
+INSERT INTO `rent` VALUES (1,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,NULL,NULL),(2,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,2,2),(3,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,3,4),(4,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,4,2),(5,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,6,3),(6,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,5,4),(7,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,2,3),(8,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,NULL,3),(9,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,4,4),(10,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,3,5),(11,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,4,3),(12,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,NULL,5),(13,'xian','changan','xianyoudian','shanxi','fangzi','adfa',NULL,NULL,NULL,3,3);
+/*!40000 ALTER TABLE `rent` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -165,7 +170,7 @@ CREATE TABLE `user` (
   `qq` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `UK_gj2fy3dcix7ph7k8684gka40c` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,6 +179,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (2,'1679211339@qq.com','leilei1','change My Password',NULL,NULL),(3,'1679211339@qq.com','leilei2','leilei',NULL,NULL),(4,'1679211339@qq.com','leilei3','leilei',NULL,NULL),(5,'1679211339@qq.com','leilei4','leilei',NULL,NULL),(6,'1679211339@qq.com','leilei5','leilei',NULL,NULL),(7,'1679211339@qq.com','leilei6','leilei',NULL,NULL);
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -186,4 +192,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-08-26 16:33:48
+-- Dump completed on 2015-08-29  8:29:05
