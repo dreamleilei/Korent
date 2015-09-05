010aeb55acbe7d35533b3389df2dc448084c74f0
lei <1679211339@qq.com>
2015-09-06 07:54:17 +0800

    修改租品的gson工具类

--- src/main/java/com/korent/util/RentGson.java
+++ src/main/java/com/korent/util/RentGson.java
@@ -1,15 +1,21 @@
 package com.korent.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * Created by lei on 15-9-2.
  */
 public class RentGson {
-    private static RentGson ourInstance = new RentGson();
+    private static Gson gson = null;
+    public RentGson() {
 
-    public static RentGson getInstance() {
-        return ourInstance;
     }
+    public static  Gson getGson() {
+        if(gson == null) {
+            gson = new GsonBuilder().setExclusionStrategies(new RentGoodsExclusionStrategy()).create();
+        }
+        return gson;
 
-    private RentGson() {
     }
 }
